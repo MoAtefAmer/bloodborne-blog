2d37c9a39db8fb4b4c99d2aad8c2595d8480f33d
MoAtefAmer <mo.atefamer@gmail.com>
2023-07-03 21:42:47 +0300

    chore: edit config

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,16 @@
 import { defineConfig } from 'vite';
 import legacy from '@vitejs/plugin-legacy';
+import { minify } from 'terser';
 
 export default defineConfig({
   base: '/bloodborne-blog/',
   plugins: [
     legacy({
-      targets: ['defaults', 'not IE 11']
-    })
+      targets: ['defaults', 'not IE 11'],
+    }),
   ],
   build: {
-    outDir: 'docs',
+    minify: 'terser',
+    // other build options...
   },
 });
