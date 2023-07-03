c800c497c455b74757d3e006ffdfbb3de36d6a10
MoAtefAmer <mo.atefamer@gmail.com>
2023-07-03 22:46:17 +0300

    config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,14 @@ import { defineConfig } from 'vite';
 import legacy from '@vitejs/plugin-legacy';
 import { terser } from 'rollup-plugin-terser';
 
+// Determine the environment mode
+const isProduction = false
+
+// Configure allowed origins based on the mode
+const allowedOrigins = isProduction
+  ? ['https://moatefamer.github.io/bloodborne-blog/']
+  : '*';
+
 export default defineConfig({
   base: '/bloodborne-blog/',
   plugins: [
@@ -15,5 +23,12 @@ export default defineConfig({
       // Terser options go here
     },
     // other build options...
+  },
+  server: {
+    cors: {
+      origin: allowedOrigins,
+      credentials: true,
+      // Add other CORS options if needed
+    },
   },
 });
